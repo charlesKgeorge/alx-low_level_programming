f2a29d186f2eff95ff2823a31d402f27c266e27b
charlesKgeorge <charleskevoregeorge@gmail.com>
2023-09-20 14:05:06 +0000

    :) (: :( ):

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,15 +9,15 @@
 int main(void)
 {
 	int i = 0;
-	long long int a = 0, b = 1;
+	unsigned long int a = 0, b = 1;
 
 	for (; i < 97; i++)
 	{
 		b += a;
 		a = b - a;
-		printf("%llu, ", b);
+		printf("%lu, ", b);
 	}
-	printf("%llu\n", a + b);
+	printf("%lu\n", a + b);
 
 	return (0);
 }
