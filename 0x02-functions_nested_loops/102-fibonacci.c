ab510e63682096e5664da95c289245f19272c2da
charlesKgeorge <charleskevoregeorge@gmail.com>
2023-09-20 13:49:33 +0000

    tell them winter came for House Frey

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,15 +8,16 @@
  */
 int main(void)
 {
-	int i = 0, a = 0, b = 1;
+	int i = 0;
+	long int a = 0, b = 1;
 
 	for (; i < 49; i++)
 	{
 		b += a;
 		a = b - a;
-		printf("%d, ", b);
+		printf("%lu, ", b);
 	}
-	printf("%d\n", a + b);
+	printf("%lu\n", a + b);
 
 	return (0);
 }
