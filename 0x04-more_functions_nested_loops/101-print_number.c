bad3cc6dfd571627999aec58bf83c06b985e7e70
charlesKgeorge <charleskevoregeorge@gmail.com>
2023-09-29 14:02:14 +0000

    123

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <limits.h>
 
 /**
  * print_number - Prints numbers to std output
@@ -8,26 +9,34 @@
  */
 void print_number(int n)
 {
-	int x = 10;
+	int x = 10, j = 0;
 
 	if (n < 0)
 	{
+		if (n == INT_MIN)
+		{
+			n++;
+			j = 1;
+		}
 		n *= -1;
 		_putchar('-');
 	}
-	while (n >= x)
+	while (n / 10 >= x)
 	{
 		x *= 10;
 	}
-	x /= 10;
 	while (x >= 1)
 	{
 		if (n == 0)
 		{
-			_putchar(n + '0');
+			_putchar('0');
 		}
 		else
 		{
+			if(x == 1)
+			{
+				n += j;
+			}
 			_putchar(n / x + '0');
 			n -= (n / x) * x;
 		}
